f2f0de08b7ee79f1b3c3727a7217e7c5b15a3d5d
Mitch Dresdner <mitchd@mindspring.com>
2017-11-17 18:32:43 -0500

    Add border pane fxml project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is now loaded!");
+    }
+
+    protected void loginCB () {
+        System.out.println("Login callback");
+    }
 }
